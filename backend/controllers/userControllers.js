cf5557051bd2a759407f5af663c830d35f4a5ba5
Andy Sen <andy.sen@icloud.com>
2023-10-11 11:58:00 -0400

    added mongodb connect, user model with bcrypt, and user register functionality

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 //! public
 
@@ -10,7 +11,29 @@ const authUser = asyncHandler(async (req, res) => {
 
 //* POST /api/users/register - register new user
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Auth User'});
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne(email);
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    };
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    };
 });
 
 //* POST /api/users/logout - logout user
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 
 import userRoutes from './routes/userRoutes.js';
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
+connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use('/api/users', userRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/users', userRoutes);
 app.get('/', (req, res) => res.send('Server is ready'));
 
 app.use(notFound);
